b8a71c2cd5bb0ef37d54c09e796922bdbad4d9d3
mahmut yuncu <mahmutla33@gmail.com>
2020-07-11 18:39:15 +0300

    First Commit

--- ios/WhatAISee/AppDelegate.h
+++ ios/WhatAISee/AppDelegate.h
@@ -1,10 +1,15 @@
-#import <Foundation/Foundation.h>
-#import <EXUpdates/EXUpdatesAppController.h>
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
 
-#import <UMCore/UMAppDelegateWrapper.h>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
-@interface AppDelegate : UMAppDelegateWrapper <RCTBridgeDelegate, EXUpdatesAppControllerDelegate>
+@property (nonatomic, strong) UIWindow *window;
 
 @end
