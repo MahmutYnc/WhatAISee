d41217cda494cc44a327f49b016a388b13361719
mahmut yuncu <mahmutla33@gmail.com>
2020-07-13 11:00:19 +0300

    first page

--- App.js
+++ App.js
@@ -1,113 +1,137 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
   StatusBar,
+  ImageBackground,
+  TouchableOpacity,
+  Alert,
+  Image,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors, ReloadInstructions} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+const App = () => {
   return (
-    <>
+    <ImageBackground
+      source={require('./images/backgrounds/Chitty.jpg')}
+      style={{width: '100%', height: '100%'}}>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>anan zaaa</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.welcome}>
+          <Text style={styles.sectionWelcomeMessage}>
+            Welcome Back <Text style={styles.highlight}>USER</Text>
+          </Text>
+          <Text style={styles.sectionStatusReport}>
+            <Text style={styles.highlight}>%75</Text> of the tasks are done!
+          </Text>
+        </View>
+        <Image
+          style={styles.image}
+          source={{
+            uri: 'http://www.reactnativeexpress.com/static/logo.png',
+          }}
+        />
+        <View style={styles.buttons}>
+          <TouchableOpacity
+            style={styles.captureImage}
+            onPress={() => Alert.alert('Left button pressed')}>
+            <Text style={{color: '#FFFFFF'}}>Capture</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.selectImage}
+            onPress={() => Alert.alert('Right button pressed')}>
+            <Text style={{color: '#FFFFFF'}}>Select Image</Text>
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
-    </>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    marginHorizontal: 16,
+    flex: 1,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  welcome: {
+    // backgroundColor: Colors.lighter,
+    width: 284,
+    height: 28,
+    left: 20,
+    top: 47,
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  sectionWelcomeMessage: {
+    // position: absolute;
+    width: 284,
+    height: 28,
+    //color: Colors.dark,
+    fontStyle: 'normal',
+    //fontWeight: 'bold',
+    fontSize: 28,
+    lineHeight: 28,
+    textAlign: 'center',
   },
   highlight: {
     fontWeight: '700',
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  sectionStatusReport: {
+    width: 190,
+    height: 16,
+    top: 70,
+
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 16,
+    lineHeight: 16,
+    textAlign: 'center',
+  },
+  image: {
+    flexDirection: 'row',
+    position: 'absolute',
+    top: 177,
+    //width: Dimensions.get('window').width - 70,
+    height: 370,
+    left: 20,
+    right: 20,
+    borderRadius: 15,
+    backgroundColor: '#FF0',
+  },
+
+  buttons: {
+    position: 'absolute',
+    paddingRight: 30,
+    paddingLeft: 0,
+    width: 351,
+    height: 30,
+    marginLeft: 30,
+    marginRight: 30,
+    top: 650,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  captureImage: {
+    //position: 'absolute',
+    width: 135,
+    height: 30,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#CA6060',
+    borderRadius: 15,
+  },
+  selectImage: {
+    //position: 'absolute',
+    width: 135,
+    height: 30,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#CA6060',
+    borderRadius: 15,
   },
 });
 
