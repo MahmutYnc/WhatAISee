e14833ce3e7506991affe6c5a27198b1593e7a92
mahmut yuncu <mahmutla33@gmail.com>
2020-07-17 00:47:56 +0300

    camera and navigation

--- App.js
+++ App.js
@@ -1,138 +1,31 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Text,
-  StatusBar,
-  ImageBackground,
-  TouchableOpacity,
-  Alert,
-  Image,
-} from 'react-native';
+// In App.js in a new project
 
-import {Colors, ReloadInstructions} from 'react-native/Libraries/NewAppScreen';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from './screens/HomeScreen';
+import Cam from './screens/CameraScreen';
 
-const App = () => {
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <ImageBackground
-      source={require('./images/backgrounds/Chitty.jpg')}
-      style={{width: '100%', height: '100%'}}>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={styles.container}>
-        <View style={styles.welcome}>
-          <Text style={styles.sectionWelcomeMessage}>
-            Welcome Back <Text style={styles.highlight}>USER</Text>
-          </Text>
-          <Text style={styles.sectionStatusReport}>
-            <Text style={styles.highlight}>%75</Text> of the tasks are done!
-          </Text>
-        </View>
-        <Image
-          style={styles.image}
-          source={{
-            uri: 'http://www.reactnativeexpress.com/static/logo.png',
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          screenOptions={{
+            headerShown: false,
           }}
         />
-        <View style={styles.buttons}>
-          <TouchableOpacity
-            style={styles.captureImage}
-            onPress={() => Alert.alert('Left button pressed')}>
-            <Text style={{color: '#FFFFFF'}}>Capture</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={styles.selectImage}
-            onPress={() => Alert.alert('Right button pressed')}>
-            <Text style={{color: '#FFFFFF'}}>Select Image</Text>
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
-    </ImageBackground>
+        <Stack.Screen name="Cam" component={Cam} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    marginHorizontal: 16,
-    flex: 1,
-  },
-  welcome: {
-    // backgroundColor: Colors.lighter,
-    width: 284,
-    height: 28,
-    left: 20,
-    top: 47,
-  },
-  sectionWelcomeMessage: {
-    // position: absolute;
-    width: 284,
-    height: 28,
-    //color: Colors.dark,
-    fontStyle: 'normal',
-    //fontWeight: 'bold',
-    fontSize: 28,
-    lineHeight: 28,
-    textAlign: 'center',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  sectionStatusReport: {
-    width: 190,
-    height: 16,
-    top: 70,
-
-    fontStyle: 'normal',
-    fontWeight: 'normal',
-    fontSize: 16,
-    lineHeight: 16,
-    textAlign: 'center',
-  },
-  image: {
-    flexDirection: 'row',
-    position: 'absolute',
-    top: 177,
-    //width: Dimensions.get('window').width - 70,
-    height: 370,
-    left: 20,
-    right: 20,
-    borderRadius: 15,
-    backgroundColor: '#FF0',
-  },
-
-  buttons: {
-    position: 'absolute',
-    paddingRight: 30,
-    paddingLeft: 0,
-    width: 351,
-    height: 30,
-    marginLeft: 30,
-    marginRight: 30,
-    top: 650,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  captureImage: {
-    //position: 'absolute',
-    width: 135,
-    height: 30,
-
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#CA6060',
-    borderRadius: 15,
-  },
-  selectImage: {
-    //position: 'absolute',
-    width: 135,
-    height: 30,
-
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#CA6060',
-    borderRadius: 15,
-  },
-});
-
+}
 export default App;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,136 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  ImageBackground,
+  TouchableOpacity,
+  Alert,
+  Image,
+} from 'react-native';
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <ImageBackground
+      source={require('../images/backgrounds/Chitty.jpg')}
+      style={{width: '100%', height: '100%'}}>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.welcome}>
+          <Text style={styles.sectionWelcomeMessage}>
+            Welcome Back <Text style={styles.highlight}>USER</Text>
+          </Text>
+          <Text style={styles.sectionStatusReport}>
+            <Text style={styles.highlight}>%75</Text> of the tasks are done!
+          </Text>
+        </View>
+        <Image
+          style={styles.image}
+          source={{
+            uri: 'http://www.reactnativeexpress.com/static/logo.png',
+          }}
+        />
+        <View style={styles.buttons}>
+          <TouchableOpacity
+            style={styles.captureImage}
+            onPress={() => navigation.push('Cam')}>
+            <Text style={{color: '#FFFFFF'}}>Capture</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.selectImage}
+            onPress={() => Alert.alert('Right button pressed')}>
+            <Text style={{color: '#FFFFFF'}}>Select Image</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 16,
+    flex: 1,
+  },
+  welcome: {
+    // backgroundColor: Colors.lighter,
+    width: 284,
+    height: 28,
+    left: 20,
+    top: 47,
+  },
+  sectionWelcomeMessage: {
+    // position: absolute;
+    width: 284,
+    height: 28,
+    //color: Colors.dark,
+    fontStyle: 'normal',
+    //fontWeight: 'bold',
+    fontSize: 28,
+    lineHeight: 28,
+    textAlign: 'center',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  sectionStatusReport: {
+    width: 190,
+    height: 16,
+    top: 70,
+
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 16,
+    lineHeight: 16,
+    textAlign: 'center',
+  },
+  image: {
+    flexDirection: 'row',
+    position: 'absolute',
+    top: 177,
+    //width: Dimensions.get('window').width - 70,
+    height: 370,
+    left: 20,
+    right: 20,
+    borderRadius: 15,
+    backgroundColor: '#FF0',
+  },
+
+  buttons: {
+    position: 'absolute',
+    paddingRight: 30,
+    paddingLeft: 0,
+    width: 351,
+    height: 30,
+    marginLeft: 30,
+    marginRight: 30,
+    top: 650,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  captureImage: {
+    //position: 'absolute',
+    width: 135,
+    height: 30,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#CA6060',
+    borderRadius: 15,
+  },
+  selectImage: {
+    //position: 'absolute',
+    width: 135,
+    height: 30,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#CA6060',
+    borderRadius: 15,
+  },
+});
+
+export default HomeScreen;
